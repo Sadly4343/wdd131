72d2f14aaec8cc6424220e0d76249ca78fa14b4d
Sadly4343 <loganhartshorn@gmail.com>
2024-06-20 16:33:44 -0600

    commit

--- scripts/temples.js
+++ scripts/temples.js
@@ -6,3 +6,11 @@ class="highlight">${today.getFullYear()}</span>`;
 
 const date = new Date(document.lastModified);
 document.getElementById("lastModified").innerHTML = date;
+
+const mainnav = document.querySelector('.navigation')
+const hambutton = document.querySelector('#menu')
+
+hambutton.addEventListener('click', () => {
+    mainnav.classList.toggle('show');
+    hambutton.classList.toggle('show');
+});
