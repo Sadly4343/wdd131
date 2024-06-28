6cf23e9fbb9fe03739fe22ef0a32a6d3386f05fc
Sadly4343 <loganhartshorn@gmail.com>
2024-06-28 11:34:58 -0600

    commit

---
+++ scripts/place.js
@@ -0,0 +1,8 @@
+const currentyear = document.querySelector("#currentyear");
+const today = new Date()
+
+currentyear.innerHTML = `@ <span
+class="highlight">${today.getFullYear()}</span>`;
+
+const date = new Date(document.lastModified);
+document.getElementById("lastModified").innerHTML = date;
