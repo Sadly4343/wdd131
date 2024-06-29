6b41f266fb1f59a66bc38f1e0a2f969f7b3f5971
Sadly4343 <loganhartshorn@gmail.com>
2024-06-28 18:05:52 -0600

    commit

--- scripts/place.js
+++ scripts/place.js
@@ -12,11 +12,11 @@ document.getElementById("lastModified").innerHTML = date;
 function calculateWindChill(temperature, wind_speed) {
     if (temperature <= 50)
         if (wind_speed >= 5)
-        return (35.74 + 0.6215 * temperature) - 35.75 * (wind_speed**0.16) + 0.4275 * (wind_speed**0.16);
+        return (35.74 + 0.6215 * temperature) - 35.75 * (wind_speed**0.16) + 0.4275 * (temperature * wind_speed**0.16);
         else 
             log.console("Incorrect")}
 
-    let value = calculateWindChill(10, 5);
+    let value = calculateWindChill(25, 5);
 
 
 document.getElementById('windchill').innerHTML = value
