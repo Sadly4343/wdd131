c47ad9af3d4e0cb1c0d9e83a5940335d2dee3c9d
Sadly4343 <loganhartshorn@gmail.com>
2024-07-13 16:37:14 -0600

    commit

--- scripts/form.js
+++ scripts/form.js
@@ -1,38 +1,60 @@
 const currentyear = document.querySelector("#currentyear");
-const today = new Date()
+const today = new Date();
 
-currentyear.innerHTML = `@ <span
-class="highlight">${today.getFullYear()}</span>`;
+currentyear.innerHTML = `@<span  class="highlight">${today.getFullYear()}</span>`;
 
 const date = new Date(document.lastModified);
 document.getElementById("lastModified").innerHTML = date;
 
 const products = [
     {
-      id: fc-1888,
+      id: 'fc-1888',
       name: "flux capacitor",
-      avg-rating: 4.5
+      'avg-rating': 4.5
     },
     {
-      id: fc-2050,
+      id: 'fc-2050',
       name: "power laces",
       averagerating: 4.7
     },
     {
-      id: fs-1987,
+      id: 'fs-1987',
       name: "time circuits",
       averagerating: 3.5
     },
     {
-      id: ac-2000,
+      id: 'ac-2000',
       name: "low voltage reactor",
       averagerating: 3.9
     },
     {
-      id: jj-1969,
+      id: 'jj-1969',
       name: "warp equalizer",
       averagerating: 5.0
     }
   ];
 
-let optionsList = document.getElementById()
+  let selectTag = document.getElementById('select-product');
+  
+  products.forEach (product => {
+      let opt = document.createElement("option");
+      opt.value = product.id; // the index
+      opt.innerHTML = product.name;
+      selectTag.append(opt);
+  });
+
+function save_data() {
+const visitShown = document.querySelector("form");
+
+let numVisits = Number(window.localStorage.getItem("visits-1s"));
+
+if (numVisits < 1) {
+  visitShown.textContent = numVisits;
+}
+  else {
+    visitShown.textContent = 'First visit';
+  
+  }
+numVisits++;
+localStorage.setItem('visits-1s', numVisits)}
+
